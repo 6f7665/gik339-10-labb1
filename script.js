f906aa2e9eca720fbe45c588d2af16b8f50fbde7
maxmatts <120714254+maxmatts@users.noreply.github.com>
2023-11-09 14:45:57 +0100

    script: add listener

--- script.js
+++ script.js
@@ -1,4 +1,31 @@
 console.log("Hej världen!");
 
+
+
+
+const checkbox = document.getElementById('divStyle');
+const textfields = document.querySelectorAll('.textfield');
+const messageBox = document.getElementById('messageBox');
+
+let hej = "Det här skrev du i";
+
+console.log(typeof(textfields));
+
+textfields.forEach((item) =>{
+    item.addEventListener('click', (e) =>{
+        
+     
+        console.log(e)
+        console.log(item.value);
+        messageBox.innerHTML = hej + " " + item.value;
+        
+    });
+});
+
+
+
 let value = document.getElementById('textbox').value;
 console.log(value);
+
+
+
